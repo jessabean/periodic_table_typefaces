8f834d85ccb3773a396d815114b5d9406addc629
jessabean <jessica.avison@gmail.com>
2013-03-24 16:29:16 -0400

    Restyle the element card

--- js/application.js
+++ js/application.js
@@ -2,20 +2,24 @@ $(document).ready(function() {
   $('li').hover(
     function () {
       var rank = $(this).data('rank');
+      var bg = $(this).data('bg');
       var name = $(this).data('title');
       var family = $(this).data('family');
       var designer = $(this).data('designer');
       var year = $(this).data('year');
 
       $('.typeface-meta').html(
+        '<div class="card">' +
         '<span class="rank">' + rank + '</span>' +
+        '<i class="' + bg + '"></i>' +
         '<span class="title">' + name + '</span>' +
+        '</div>' +
         '<span class="family">' + family + '</span>' +
         '<span class="designer">' + designer + '</span>' +
         '<span class="year">' + year + '</span>'
       );
 
-      $('.typeface-meta').toggleClass('is-hidden');
+      $('.typeface-meta').toggleClass('is-hidden ');
     }
   );
 });
