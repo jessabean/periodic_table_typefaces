e53d220fd0d4a53e930e0c0a8f1b08a116d251e9
jessabean <jessica.avison@gmail.com>
2016-06-17 13:26:58 -0400

    Stub in meta information on click

--- js/application.js
+++ js/application.js
@@ -22,6 +22,33 @@ var showMeta = function(event){
   $('.typeface-meta').toggleClass('is-hidden');
 };
 
+var showMetaFullScreen = function(event) {
+  var target = event.target;
+
+  var rank      = $(target).data('rank');
+  var bg        = $(target).data('bg');
+  var name      = $(target).data('title');
+  var family    = $(target).data('family');
+  var designer  = $(target).data('designer');
+  var year      = $(target).data('year');
+
+  $('.typeface-meta-fullscreen').html(
+    '<div class="card">' +
+    '<span class="rank">' + rank + '</span>' +
+    '<i class="' + bg + '"></i>' +
+    '<span class="title">' + name + '</span>' +
+    '</div>' +
+    '<span class="family">' + family + '</span>' +
+    '<span class="designer">' + designer + '</span>' +
+    '<span class="year">' + year + '</span>'
+  );
+
+  $('.typeface-meta-fullscreen').toggleClass('is-hidden');
+
+};
+
 $( document ).ready(function() {
   $("li.typeface").hover(showMeta);  
+  $(document).on("click", "li.typeface", showMetaFullScreen);
 });
+
