6317629740c5dc78f078bad8d6d572efba12919e
jessabean <jessica.avison@gmail.com>
2016-06-21 14:48:52 -0400

    Watch JS files

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ gulp.task('express', function() {
 
 gulp.task('watch', function () {
     gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['js/*.js', 'js/*.json'], ['browser-reload']);
     gulp.watch(['*.html', '_layouts/*.html', 'templates/*.html'], ['browser-reload']);
 });
 
