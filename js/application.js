d15849c8791660ca558eb5729f083a7bc766da99
jessabean <jessica.avison@gmail.com>
2016-06-21 14:59:25 -0400

    Move everything out of the test file and into index

--- js/application.js
+++ js/application.js
@@ -19,45 +19,60 @@ var showMeta = function(event){
   $('.typeface-meta').toggleClass('is-hidden');
 };
 
+// Build the typeface meta modal and toggle visibility
 var showMetaFullScreen = function(event) {
-  var target = event.target;
+  var target = event.target; // li.typeface
 
-  var rank      = $(target).data('rank');
-  var bg        = $(target).data('bg');
-  var name      = $(target).data('title');
-  var family    = $(target).data('family');
-  var designer  = $(target).data('designer');
-  var year      = $(target).data('year');
+  $.get('templates/typeface-meta.mustache.html',
+    function (template, textStatus, jqXhr) {
 
-  $('.typeface-meta-fullscreen').html(
-    '<div class="modal">' +
-      '<div class="modal__actions">' +
-        '<button class="modal__close" type="button"><i class="ico ico-close"></i><span class="visually-hidden">close</span></button>' +
-      '</div>' +
-      '<div class="media">' +
-        '<div class="media__figure">' +
-          '<div class="card">' +
-            '<span class="rank">' + rank + '</span>' +
-            '<i class="' + bg + '"></i>' +
-            '<span class="title">' + name + '</span>' +
-          '</div>' +
-        '</div>' +
-      '<div class="media__body">' +
-        '<span class="family">' + family + '</span>' +
-        '<span class="designer">' + designer + '</span>' +
-        '<span class="year">' + year + '</span>' +
-      '</div>' +
-    '</div>'
-  );
+      var typefaceData = {
+        name: $(target).data('title'),
+        abbreviation: $(target).textContent,
+        rank: $(target).data('rank'),
+        family: $(target).data('family'),
+        designer: $(target).data('designer'),
+        year: $(target).data('year')
+      }
+
+      typefaceData.lower = function () {
+        return function (text, render) {
+          text = render(text).replace(/\é/g,"e");
+          return render(text).replace(/(\.\s)|\s/g, '-').toLowerCase();
+        }
+      };
+   
+    var typefaceMetaHtml = Mustache.render(template, typefaceData);
+    $('.typeface-meta-fullscreen').html(typefaceMetaHtml);
+  });
 
   $('.typeface-meta-fullscreen').toggleClass('is-active');
-
 };
 
 $( document ).ready(function() {
+  // Build the typefaces table from JSON file
+  $.getJSON('./js/typefaces.json', {}, function (typefaceData, textStatus, jqXHr) {
+     $.get('templates/typeface-table.mustache.html',
+     function (template, textStatus, jqXhr) {
+      
+      typefaceData.lower = function () {
+        return function (text, render) {
+          text = render(text).replace(/\é/g,"e");
+          return render(text).replace(/(\.\s)|\s/g, '-').toLowerCase();
+        }
+      };
+
+       var typefaceTableHtml = Mustache.render(template, typefaceData);
+       $('#main').prepend(typefaceTableHtml);
+     });
+   });
+
   $("li.typeface").hover(showMeta);  
+
   $(document).on("click", "li.typeface", showMetaFullScreen);
+
   $(document).on("click", ".modal__close", function(){
     $('.modal-container').removeClass('is-active');
   })
+
 });
